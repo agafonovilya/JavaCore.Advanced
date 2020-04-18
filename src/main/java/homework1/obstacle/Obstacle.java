62ffebd647f8a913d380336780bcd1632683f742
agafonovilya <agafonov.i.m@gmail.com>
2020-04-18 15:07:31 +0300

    Delete files

--- src/main/java/homework1/obstacle/Obstacle.java
+++
@@ -1,8 +0,0 @@
-package homework1.obstacle;
-
-import homework1.participant.Participant;
-
-public interface Obstacle {
-
-    boolean passObstacle(Participant participant); //Метод преодоления препятствия.
-}
